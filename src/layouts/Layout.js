e4986ee4c94c02534bb79e01728d499fb6f077c0
An nayoung <any92nabee@gmail.com>
2022-11-18 19:27:40 +0900

    [안나영] 메인틀

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,21 +2,22 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/common/Header";
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
-import App from "../App";
 import LayoutCSS from "./Layout.module.css"
 
 function Layout() {
 
     return (
         <>
-            <Header/>
-            <Navbar/>
-            <main className={ LayoutCSS.main}>
-                <Outlet/>
-            </main>
-            <Footer/>
+            <div className="wrapper">
+                <Header/>
+                <Navbar/>
+                <main className={ LayoutCSS.main}>
+                    <Outlet/>
+                </main>
+                <Footer/>
+            </div>  
         </>
     );
 }
 
-export default App;
+export default Layout;
