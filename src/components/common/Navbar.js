98cf7486c8c0d0ae0375d8d4b8018cdd50067a74
kimdonghyun <donghyun9191@gmail.com>
2022-11-21 17:57:15 +0900

    commit

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,15 +1,49 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+
 import NavCSS from './Navbar.module.css';
+import { decodeJwt } from '../../utils/tokenUtils';
+import {useRef, useState} from "react";
+//import {classNames} from "classNames";
+import useDetectClose from "../../hooks/useDetectCLose";
+
 
 function Navbar(){
+
+    //const dropDownRef = useRef(null);
+    //const [isOpen, setIsOpen] = useDetectClose(dropDownRef, false);
+    const isLogin = window.localStorage.getItem('accessToken');
+    let decoded = null;
+
+    if(isLogin){
+        const temp = decodeJwt(isLogin);
+        decoded = temp.auth[0];
+    }
 
     return(
         <div className={ NavCSS.NavbarDiv }>
             <ul className={ NavCSS.NavlistUl }>
-                <li><NavLink to="/"><button>공지사항</button></NavLink></li>
-                <li><NavLink to="/"><button>내 정보</button></NavLink></li>
-                <li><NavLink to="/"><button>내 강의</button></NavLink></li>
-                <li><NavLink to="/"><button>1:1 상담</button></NavLink></li>
+                { decoded === "ROLE_ADMIN" &&<li><NavLink to="/"><button className={NavCSS.dropbtn}>공지사항</button>
+                <div className={ NavCSS.dropdowncontent }>
+    <a href="#">12321</a>
+    <a href="#">L312321</a>
+    <a href="#">Link3</a>
+  </div></NavLink></li>}
+                { decoded === "ROLE_ADMIN" &&<li><NavLink to="/"><button>원생관리</button></NavLink></li>}
+                { decoded === "ROLE_ADMIN" &&<li><NavLink to="/"><button>강사관리</button></NavLink></li>}
+                { decoded === "ROLE_ADMIN" &&<li><NavLink to="/"><button>강의관리</button></NavLink></li>}
+                { decoded === "ROLE_ADMIN" &&<li><NavLink to="/"><button>수납관리</button></NavLink></li>}
+                { decoded === "ROLE_ADMIN" &&<li><NavLink to="/"><button>상담관리</button></NavLink></li>}
+                {/* 강사 */}
+                { decoded === "ROLE_TEACHER" &&<li><NavLink to="/"><button>공지사항</button></NavLink></li>}
+                { decoded === "ROLE_TEACHER" &&<li><NavLink to="/"><button>강의관리</button></NavLink></li>}
+                { decoded === "ROLE_TEACHER" &&<li><NavLink to="/"><button>원생관리</button></NavLink></li>}
+                { decoded === "ROLE_TEACHER" &&<li><NavLink to="/"><button>1:1 상담</button></NavLink></li>}
+                {/* 원생 */}
+                { decoded === "ROLE_STUDENT" && <li><NavLink to="/"><button>공지사항</button></NavLink></li>}
+                { decoded === "ROLE_STUDENT" && <li><NavLink to="/"><button>내 정보</button></NavLink></li>}
+                { decoded === "ROLE_STUDENT" && <li><NavLink to="/"><button>내 강의</button></NavLink></li>}
+                { decoded === "ROLE_STUDENT" && <li><NavLink to="/"><button>1:1 상담</button></NavLink></li>}
+                
             </ul>
         </div>
     )
