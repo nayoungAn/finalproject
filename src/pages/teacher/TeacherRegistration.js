830530d25f26a62fa5c561e390d129d7f5bd2b38
yunji86 <rhaehfdlvn91@gmail.com>
2022-11-23 16:43:04 +0900

    [김윤지] 강사등록 수정 중

--- src/pages/teacher/TeacherRegistration.js
+++ src/pages/teacher/TeacherRegistration.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { callTeacherRegistAPI } from "../../api/TeacherAPICall";
-
+import teacherRegistrationCSS from './TeacherRegistration.module.css';
 function TeacherRegistration(){
 
     const dispatch = useDispatch();
@@ -11,14 +11,14 @@ function TeacherRegistration(){
     const [ image, setImage ] = useState(null);
     const [ imageUrl, setImageUrl] = useState('');
     const [ form, setForm] = useState({
+        memberName: '',
+        memberBirthday: '',
         memberId : '',
         memberPassword: '',
-        memberName: '',
-        memberPhone: '',
-        memberBirthday: '',
         memberGender: '',
-        memberAddress: '',
-        memberEmail: ''
+        memberEmail: '',
+        memberPhone: '',
+        memberAddress: ''
     });
 
     const onClickImageUpload = () => {
@@ -73,6 +73,7 @@ function TeacherRegistration(){
             form : formData
         }));
 
+        alert('강사 등록 완료');
         navigate('/ono/teacher', { replace : true });
         window.location.reload();
     }
@@ -93,85 +94,87 @@ function TeacherRegistration(){
                     onChange={ onChangeImageUpload }   
                     ref={ imageInput }
                 />
-                <img
-                    src ='img/memimg.png'
-                    onClick={ onClickImageUpload }   
-                /> 
+                <button
+                    onClick={ onClickImageUpload }
+                > 업로드 </button>
+                
             </div>
             <div>
                 <table>
-                    <tr>
-                        <td><label> 이름 </label></td>
-                        <td>
-                            <input
-                                name='memberName'
-                                onChange={ onChangeHandler }
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> 생년월일 </label></td>
-                        <td>
-                            <input
-                                name='memberBirthday'
-                                onChange={ onChangeHandler }
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> 아이디 </label></td>
-                        <td>
-                            <input
-                                name='memberId'
-                                onChange={ onChangeHandler }
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> 비밀번호 </label></td>
-                        <td>
-                            <input
-                                name='memberPassword'
-                                onChange={ onChangeHandler }
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> 성별 </label></td>
-                        <td>
-                            <label><input type="radio" name="memberGender"  onChange={ onChangeHandler } value="여성"/> 여성</label> &nbsp;
-                            <label><input type="radio" name="memberGender"  onChange={ onChangeHandler } value="남성"/> 남성</label>
-                       
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> 이메일 </label></td>
-                        <td>
-                            <input
-                                name='memberEmail'
-                                onChange={ onChangeHandler }
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> 휴대번호 </label></td>
-                        <td>
-                            <input
-                                name='memberPhone'
-                                onChange={ onChangeHandler }
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> 주소 </label></td>
-                        <td>
-                            <input
-                                name='memberAddress'
-                                onChange={ onChangeHandler }
-                            />
-                        </td>
-                    </tr>
-
+                    <tbody>
+                        <tr>
+                            <td><label> 이름 </label></td>
+                            <td>
+                                <input
+                                    name='memberName'
+                                    onChange={ onChangeHandler }
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> 생년월일 </label></td>
+                            <td>
+                                <input
+                                    name='memberBirthday'
+                                    onChange={ onChangeHandler }
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> 아이디 </label></td>
+                            <td>
+                                <input
+                                    name='memberId'
+                                    onChange={ onChangeHandler }
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> 비밀번호 </label></td>
+                            <td>
+                                <input
+                                    type='password'
+                                    name='memberPassword'
+                                    onChange={ onChangeHandler }
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> 성별 </label></td>
+                            <td>
+                                <label><input type="radio" name="memberGender"  onChange={ onChangeHandler } value="여성"/> 여성</label> &nbsp;
+                                <label><input type="radio" name="memberGender"  onChange={ onChangeHandler } value="남성"/> 남성</label>
+                        
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> 이메일 </label></td>
+                            <td>
+                                <input
+                                    name='memberEmail'
+                                    onChange={ onChangeHandler }
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> 휴대번호 </label></td>
+                            <td>
+                                <input
+                                    name='memberPhone'
+                                    onChange={ onChangeHandler }
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> 주소 </label></td>
+                            <td>
+                                <input
+                                    name='memberAddress'
+                                    onChange={ onChangeHandler }
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
             <button
